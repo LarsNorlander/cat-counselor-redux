86823f6ac317f650f6071a02030b3573a5e0792c
Lars Norlander <lars.nrlndr@gmail.com>
2018-04-30 15:45:16 +0800

    Initial commit.

---
+++ src/main/kotlin/com/larsnorlander/catcounselorredux/CatCounselorReduxApplication.kt
@@ -0,0 +1,11 @@
+package com.larsnorlander.catcounselorredux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatCounselorReduxApplication
+
+fun main(args: Array<String>) {
+    runApplication<CatCounselorReduxApplication>(*args)
+}
