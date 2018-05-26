62a210688719e85bf9786de0419dc2bea8b5ab0b
Lars Norlander <lars.nrlndr@gmail.com>
2018-05-26 15:12:38 +0800

    Cleaned up ComputeMatchesAndMissesTest

--- src/test/kotlin/com/larsnorlander/catcounselorredux/counseling/ComputeMatchesAndMissesTest.kt
+++ src/test/kotlin/com/larsnorlander/catcounselorredux/counseling/ComputeMatchesAndMissesTest.kt
@@ -8,37 +8,44 @@ import org.junit.Test
 
 class ComputeMatchesAndMissesTest {
 
+    private companion object {
+        private const val SCIENCE = "Science"
+        private const val MATH = "Math"
+        private const val HISTORY = "History"
+        private const val COMPUTER = "Computer"
+    }
+
     @Test
     fun `perfect match`() {
-        val strengths = setOf("Science", "Math")
-        val requirements = setOf("Science", "Math")
+        val subjects = setOf(SCIENCE, MATH)
 
-        val result = strengths.computeMatchesAndMissesFor(requirements)
+        val result = subjects.computeMatchesAndMissesFor(subjects)
 
-        assertThat(result.matches, `is`(equalTo(setOf("Science", "Math"))))
+        assertThat(result.matches, `is`(equalTo(subjects)))
         assertThat(result.misses, `is`(empty()))
     }
+
 
     @Test
     fun `no match`() {
         val strengths = emptySet<String>()
-        val requirements = setOf("Science", "Math")
+        val requirements = setOf(SCIENCE, MATH)
 
         val result = strengths.computeMatchesAndMissesFor(requirements)
 
-        assertThat(result.misses, `is`(equalTo(setOf("Science", "Math"))))
+        assertThat(result.misses, `is`(equalTo(requirements)))
         assertThat(result.matches, `is`(empty()))
     }
 
     @Test
     fun `one match out of three requirements`() {
-        val strengths = setOf("Science", "History")
-        val requirements = setOf("Science", "Math", "Computer")
+        val strengths = setOf(SCIENCE, HISTORY)
+        val requirements = setOf(SCIENCE, MATH, COMPUTER)
 
         val result = strengths.computeMatchesAndMissesFor(requirements)
 
-        assertThat(result.matches, `is`(equalTo(setOf("Science"))))
-        assertThat(result.misses, `is`(equalTo(setOf("Math", "Computer"))))
+        assertThat(result.matches, `is`(equalTo(setOf(SCIENCE))))
+        assertThat(result.misses, `is`(equalTo(setOf(MATH, COMPUTER))))
     }
 }
 
