c6cdf3c255a1b6ce0915d1c8c701b6f586750ad6
Lars Norlander <lars.nrlndr@gmail.com>
2018-06-04 08:37:35 +0800

    Cleaned up the tests a bit for now.

--- src/test/kotlin/com/larsnorlander/catcounselorredux/counselor/CounselorTest.kt
+++ src/test/kotlin/com/larsnorlander/catcounselorredux/counselor/CounselorTest.kt
@@ -9,83 +9,104 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.greaterThan
 import org.junit.Test
 
-class CounselorTest {
+private const val STEM = "STEM"
+private const val ABM = "ABM"
+private const val ANY_STRAND_NAME = "any strand"
 
-    private val anyStrand = "any strand"
-    private val anyCriterion: Criterion = "any criterion"
+private const val ANY_CRITERION: Criterion = "any criterion"
+
+private const val UPPER_BOUND_SCORE = 1.0
+private const val LOWER_BOUND_SCORE = 0.0
+
+class CounselorTest {
 
     @Test
     fun `get statistics for strand with 1 match and 1 miss for 1 criterion`() {
-        val strand = Strand(name = anyStrand, requirements = mapOf(anyCriterion to setOf("A", "B")))
-        val specification = listOf(strand)
-        val records: Map<Criterion, Map<Item, Score>> = mapOf(anyCriterion to mapOf("A" to 1.0, "B" to 0.0))
+        val specification = listOf(
+                Strand(name = ANY_STRAND_NAME, requirements = mapOf(ANY_CRITERION to setOf("A", "B"))))
+        val records: Map<Criterion, Map<Item, Score>> = mapOf(
+                ANY_CRITERION to mapOf(
+                        "A" to UPPER_BOUND_SCORE,
+                        "B" to LOWER_BOUND_SCORE))
 
         val result: Map<Criterion, Statistics> = Counselor(specification).computeStatistics(
-                strandName = anyStrand,
+                strandName = ANY_STRAND_NAME,
                 records = records)
 
-        assertThat(result[anyCriterion]!!.matches, `is`(setOf("A")))
-        assertThat(result[anyCriterion]!!.misses, `is`(setOf("B")))
+        assertThat(result[ANY_CRITERION]!!.matches, `is`(setOf("A")))
+        assertThat(result[ANY_CRITERION]!!.misses, `is`(setOf("B")))
     }
 
     @Test
     fun `average grade is included in strengths`() {
-        val strand = Strand(name = anyStrand, requirements = mapOf(anyCriterion to setOf("A", "B")))
-        val specification = listOf(strand)
-        val records: Map<Criterion, Map<Item, Score>> = mapOf(anyCriterion to mapOf("A" to 1.0, "B" to 1.0))
+        val specification = listOf(
+                Strand(name = ANY_STRAND_NAME, requirements = mapOf(ANY_CRITERION to setOf("A", "B"))))
+        val records: Map<Criterion, Map<Item, Score>> = mapOf(
+                ANY_CRITERION to mapOf(
+                        "A" to UPPER_BOUND_SCORE,
+                        "B" to UPPER_BOUND_SCORE))
 
         val result: Map<Criterion, Statistics> = Counselor(specification).computeStatistics(
-                strandName = anyStrand,
+                strandName = ANY_STRAND_NAME,
                 records = records)
 
-        assertThat(result[anyCriterion]!!.matches, `is`(setOf("A", "B")))
+        assertThat(result[ANY_CRITERION]!!.matches, `is`(setOf("A", "B")))
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun `throws exception for non-existent strand`() {
-        Counselor(emptyList()).computeStatistics(anyStrand, emptyMap())
+        Counselor(emptyList()).computeStatistics(ANY_STRAND_NAME, emptyMap())
     }
 
     @Test
     fun `strand with more matches scores higher`() {
-        val stem = Strand(name = "STEM", requirements = mapOf(anyCriterion to setOf("A", "B")))
-        val abm = Strand(name = "ABM", requirements = mapOf(anyCriterion to setOf("A", "C")))
-        val specification = listOf(stem, abm)
-        val scores: Map<Item, Score> = mapOf("A" to 1.0, "B" to 1.0, "C" to 0.0)
+        val specification = listOf(
+                Strand(name = STEM, requirements = mapOf(ANY_CRITERION to setOf("A", "B"))),
+                Strand(name = ABM, requirements = mapOf(ANY_CRITERION to setOf("A", "C"))))
+        val scores: Map<Item, Score> = mapOf(
+                "A" to UPPER_BOUND_SCORE,
+                "B" to UPPER_BOUND_SCORE,
+                "C" to LOWER_BOUND_SCORE)
 
         val strandScores: Map<String, Score> = Counselor(specification).scoreStrands(
-                criterion = anyCriterion,
+                criterion = ANY_CRITERION,
                 scores = scores)
 
-        assertThat(strandScores["STEM"]!!, `is`(greaterThan(strandScores["ABM"]!!)))
+        assertThat(strandScores[STEM]!!, `is`(greaterThan(strandScores[ABM]!!)))
     }
 
     @Test
     fun `strand with equal matches but less misses scores higher`() {
-        val stem = Strand(name = "STEM", requirements = mapOf(anyCriterion to setOf("A", "B", "C")))
-        val abm = Strand(name = "ABM", requirements = mapOf(anyCriterion to setOf("A", "B")))
-        val specification = listOf(stem, abm)
-        val scores: Map<Item, Score> = mapOf("A" to 1.0, "B" to 1.0, "C" to 0.0)
+        val specification = listOf(
+                Strand(name = STEM, requirements = mapOf(ANY_CRITERION to setOf("A", "B", "C"))),
+                Strand(name = ABM, requirements = mapOf(ANY_CRITERION to setOf("A", "B"))))
+        val scores: Map<Item, Score> = mapOf(
+                "A" to UPPER_BOUND_SCORE,
+                "B" to UPPER_BOUND_SCORE,
+                "C" to LOWER_BOUND_SCORE)
 
         val strandScores: Map<String, Score> = Counselor(specification).scoreStrands(
-                criterion = anyCriterion,
+                criterion = ANY_CRITERION,
                 scores = scores)
 
-        assertThat(strandScores["ABM"]!!, `is`(greaterThan(strandScores["STEM"]!!)))
+        assertThat(strandScores[ABM]!!, `is`(greaterThan(strandScores[STEM]!!)))
     }
 
     @Test
     fun `strands with equal matches and misses score the same`() {
-        val stem = Strand(name = "STEM", requirements = mapOf(anyCriterion to setOf("A", "B", "C")))
-        val abm = Strand(name = "ABM", requirements = mapOf(anyCriterion to setOf("A", "B", "C")))
-        val specification = listOf(stem, abm)
-        val scores: Map<Item, Score> = mapOf("A" to 1.0, "B" to 1.0, "C" to 0.0)
+        val specification = listOf(
+                Strand(name = STEM, requirements = mapOf(ANY_CRITERION to setOf("A", "B", "C"))),
+                Strand(name = ABM, requirements = mapOf(ANY_CRITERION to setOf("A", "B", "C"))))
+        val scores: Map<Item, Score> = mapOf(
+                "A" to UPPER_BOUND_SCORE,
+                "B" to UPPER_BOUND_SCORE,
+                "C" to LOWER_BOUND_SCORE)
 
         val strandScores: Map<String, Score> = Counselor(specification).scoreStrands(
-                criterion = anyCriterion,
+                criterion = ANY_CRITERION,
                 scores = scores)
 
-        assertThat(strandScores["ABM"]!!, `is`(equalTo(strandScores["STEM"]!!)))
+        assertThat(strandScores[ABM]!!, `is`(equalTo(strandScores[STEM]!!)))
     }
 }
 
