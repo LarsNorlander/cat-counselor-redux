f90b69a5a9aca7892b321947e1f8a15f8223bcb2
Lars Norlander <lars.nrlndr@gmail.com>
2018-06-07 17:38:13 +0800

    Implemented method for getting all items with tests.

--- src/main/kotlin/com/larsnorlander/catcounselorredux/counselor/Specification.kt
+++ src/main/kotlin/com/larsnorlander/catcounselorredux/counselor/Specification.kt
@@ -4,8 +4,19 @@ import Criterion
 import Item
 
 data class Specification(val strands: List<Strand>) {
-    val items: Map<Criterion, Item>
+    val items: Map<Criterion, Set<Item>>
         get() {
-            TODO()
+            val result = mutableMapOf<Criterion, MutableSet<Item>>()
+            for (strand in strands) {
+                for (criterion in strand.requirements.keys) {
+                    if (result[criterion] != null)
+                        result[criterion]!!.addAll(strand.requirements[criterion]!!)
+                    else {
+                        result[criterion] = mutableSetOf()
+                        result[criterion]!!.addAll(strand.requirements[criterion]!!)
+                    }
+                }
+            }
+            return result
         }
 }
--- src/test/kotlin/com/larsnorlander/catcounselorredux/counselor/SpecificationTest.kt
+++ src/test/kotlin/com/larsnorlander/catcounselorredux/counselor/SpecificationTest.kt
@@ -15,8 +15,27 @@ class SpecificationTest {
                         "Grades" to setOf(),
                         "NCAE" to setOf()))))
 
-        val items: Map<Criterion, Item> = specification.items
+        val items: Map<Criterion, Set<Item>> = specification.items
 
         assertThat(items.keys, `is`(equalTo(setOf("Grades", "NCAE"))))
+    }
+
+    @Test
+    fun `collect all items from one criteria over several strands`() {
+        val specification = Specification(listOf(
+                Strand(name = "STEM", requirements = mapOf(
+                        "Grades" to setOf("A", "B"),
+                        "NCAE" to setOf("X")
+                )),
+                Strand(name = "ABM", requirements = mapOf(
+                        "Grades" to setOf("B", "C"),
+                        "NCAE" to setOf("Y", "Z")
+                ))
+        ))
+
+        val items: Map<Criterion, Set<Item>> = specification.items
+
+        assertThat(items["Grades"], `is`(equalTo(setOf("A", "B", "C"))))
+        assertThat(items["NCAE"], `is`(equalTo(setOf("X", "Y", "Z"))))
     }
 }
