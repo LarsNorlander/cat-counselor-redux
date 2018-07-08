2c41f93ad7f556a79bed79e9d279744103af5006
Lars Norlander <lars.nrlndr@gmail.com>
2018-07-08 15:06:46 +0800

    Made a proper mapping for items. Added mapping for getting the specification.

--- src/main/kotlin/com/larsnorlander/catcounselorredux/counselor/spring/rest/SpecificationRestController.kt
+++ src/main/kotlin/com/larsnorlander/catcounselorredux/counselor/spring/rest/SpecificationRestController.kt
@@ -3,6 +3,7 @@ package com.larsnorlander.catcounselorredux.counselor.spring.rest
 import Criterion
 import Item
 import com.larsnorlander.catcounselorredux.counselor.Specification
+import com.larsnorlander.catcounselorredux.counselor.Strand
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 class SpecificationRestController(val specification: Specification) {
 
     @GetMapping
+    fun getStrands(): ResponseEntity<List<Strand>> {
+        return ResponseEntity.ok(specification.strands)
+    }
+
+    @GetMapping("items")
     fun getSpecification(): ResponseEntity<Map<Criterion, Set<Item>>> {
         return ResponseEntity.ok(specification.items)
     }
